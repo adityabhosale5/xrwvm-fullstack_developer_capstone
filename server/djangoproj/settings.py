f833a9287b541a61aaa98ad5d2b0f93d6991e6fc
aditya <adityabhosale5247@g,ail.com>
2024-11-12 15:33:12 -0500

    Fixes-6

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,11 +29,17 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost', 
-    'https://aditya222202-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'localhost',
+    (
+        'https://aditya222202-8000.theiadockernext-1-'
+        'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    )
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://aditya222202-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'https://aditya222202-8000.theiadockernext-1-'
+        'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    )
 ]
 
 REST_FRAMEWORK = {
@@ -99,7 +105,10 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        (
+            'django.contrib.auth.password_validation'
+            '.UserAttributeSimilarityValidator'
+        ),
     },
     {
         'NAME':
